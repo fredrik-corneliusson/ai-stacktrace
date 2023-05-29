b6bfdf3cf521deacc723143094f591e9c370fa11
Fredrik Corneliusson <fredrik.corneliusson@gmail.com>
2023-05-30 01:09:09 +0200

    Fix issue with callback not working first time.

--- svelte-app/src/Callback.svelte
+++ svelte-app/src/Callback.svelte
@@ -3,13 +3,23 @@
     import {navigate} from "svelte-routing";
 
     onMount(() => {
-        // Here you can handle the callback. For example, parse the token from the URL
-        const urlParams = new URLSearchParams(window.location.hash);
-        const token = urlParams.get("#access_token");
+        const hashParams = window.location.hash
+            .substring(1)
+            .split('&')
+            .reduce(function (initial, item) {
+                if (item) {
+                    var parts = item.split('=');
+                    initial[parts[0]] = decodeURIComponent(parts[1]);
+                }
+                return initial;
+            }, {});
+        const token = hashParams.access_token;
         console.log("Got callback");
         console.log(token);
         // Do something with the token, like storing it for future use
-        localStorage.setItem('token', token);
+        if (token) {
+            localStorage.setItem('token', token);
+        }
         // Navigate to /main
         navigate('/');
     });
